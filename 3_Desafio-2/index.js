db7ea93cb2ce610fa5b2775d920ffc9a95fa5b4f
Rafael <rr65887@gmail.com>
2023-04-10 18:51:59 -0300

    enviando desafio 2

---
+++ 3_Desafio-2/index.js
@@ -0,0 +1,5 @@
+var num = 10;
+var numString = num.toString();
+console.log(num);
+console.log(numString + " é uma string");
+console.log(typeof numString);
---
+++ 3_Desafio-2/index.ts
@@ -0,0 +1,9 @@
+let num: number = 10
+
+let numString = num.toString()
+
+console.log(num)
+
+console.log(numString + " é uma string")
+
+console.log(typeof numString)
