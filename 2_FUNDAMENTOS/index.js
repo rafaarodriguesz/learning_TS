5e84508b83a9c4ee528773f9f2b876d5efc351f2
Rafael <rr65887@gmail.com>
2023-04-10 18:08:29 -0300

    enviando projeto 2

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,9 @@
+// 1 - numbers
+var xa = 10;
+console.log(xa);
+xa = 16;
+console.log(typeof xa);
+var yz = 15.2354325;
+console.log(typeof yz);
+console.log(yz);
+console.log(yz.toPrecision(3));
---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,17 @@
+// 1 - numbers
+
+let xa:number = 10
+
+console.log(xa);
+
+xa = 16
+
+console.log(typeof xa)
+
+const yz:number = 15.2354325
+
+console.log(typeof yz)
+
+console.log(yz)
+
+console.log(yz.toPrecision(3))
