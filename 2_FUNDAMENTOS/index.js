a4d2acc8d38d00db39db037907794be359aeaf27
Rafael <rr65887@gmail.com>
2023-04-10 18:16:55 -0300

    enviando final

--- 2_FUNDAMENTOS/index.js
+++ 2_FUNDAMENTOS/index.js
@@ -7,3 +7,17 @@ var yz = 15.2354325;
 console.log(typeof yz);
 console.log(yz);
 console.log(yz.toPrecision(3));
+// 2 - string
+var firstName = "Matheus";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Batisti";
+fullName = firstName + " " + lastName;
+console.log(typeof fullName);
+console.log(fullName);
+// 3 - Boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -15,3 +15,30 @@ console.log(typeof yz)
 console.log(yz)
 
 console.log(yz.toPrecision(3))
+
+// 2 - string
+
+const firstName: string = "Matheus"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Batisti"
+
+fullName = firstName + " " + lastName
+
+console.log(typeof fullName)
+ 
+console.log(fullName)
+
+// 3 - Boolean
+
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true 
+
+console.log(a)
