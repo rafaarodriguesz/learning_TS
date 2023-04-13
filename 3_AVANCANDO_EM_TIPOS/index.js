f8502083faf1623bb70229e485c9da7940716abf
Rafael <rr65887@gmail.com>
2023-04-12 22:00:20 -0300

    sessao 3

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,12 @@
+// 1 - arrays
+var number = [1, 2, 3, 4];
+number.push(5);
+console.log(number[2]);
+var nome = ["Joao", "mairia", "josé"];
+nome.push("Joao");
+//2 - sintaxe diferente
+var nums = [100, 200, 300];
+nums.push(300);
+console.log(nums);
+// nums.push("teste")
+console.log(nums[0]);
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,24 @@
+// 1 - arrays
+let number: number[] = [1,2,3,4]
+
+number.push(5)
+
+console.log(number[2])
+
+const nome: string[] = ["Joao","mairia","josé"]
+
+nome.push("Joao")
+
+//2 - sintaxe diferente
+
+const nums: Array<number> = [100, 200, 300]
+
+nums.push(300)
+
+console.log(nums)
+
+// nums.push("teste")
+
+console.log(nums[0])
+
+// 3 - any qualquer tipo satisfaz
