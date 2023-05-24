fb8a03fd6a674160ec1c865b6104ab4181b6216f
Rafael <rr65887@gmail.com>
2023-05-23 21:58:51 -0300

    enviando tudo

--- 9_Modules/index.ts
+++ 9_Modules/index.ts
@@ -8,3 +8,43 @@ importGreet()
 import {x} from "./variavle.js"
 
 console.log(x)
+
+// 3 multiplas importações 
+import {a, b, myFunction } from "./multiple.js"
+
+console.log(a)
+console.log(b) 
+
+myFunction();
+
+// 4 alias
+
+import { someName as name } from "./changeName.js"
+
+console.log(name);
+
+// 5 importar tudo
+import * as myNumbers from "./numbers.js"
+
+const n3 = myNumbers.n3
+
+console.log(myNumbers)
+console.log(n3)
+
+// 6 importar tipos 
+import {Human} from "./mytype"
+
+class User implements Human{
+    name
+    age
+
+    constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+    }
+}
+
+const joao = new User ("João", 25)
+
+console.log(joao)
+
